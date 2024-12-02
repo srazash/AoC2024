9576314d36c7353b2a067830da41d8d481013520
Ryan Shaw-Harrison <srazash@protonmail.com>
2024-12-02 21:03:38 +0000

    Day 2 Puzzle 1 complete, debugging Day 2 Puzzle 2

--- day02/puzzle1.go
+++ day02/puzzle1.go
@@ -11,5 +11,41 @@ func Puzzle1() {
 
 	l := aocutil.Day02Input("day02/input.txt")
 
-	fmt.Println(len(l))
+	total := 0
+
+	for _, v := range l {
+		if safe(v) {
+			total += 1
+		}
+	}
+
+	fmt.Printf("\tAnswer = %d\n", total)
+}
+
+func safe(input []int) bool {
+	stepUp := input[0] < input[1]
+
+	for i := range input {
+		if i+1 == len(input) {
+			break
+		}
+		if stepUp {
+			if input[i] > input[i+1] {
+				return false
+			}
+			diff := input[i+1] - input[i]
+			if diff < 1 || diff > 3 {
+				return false
+			}
+		} else {
+			if input[i] < input[i+1] {
+				return false
+			}
+			diff := input[i] - input[i+1]
+			if diff < 1 || diff > 3 {
+				return false
+			}
+		}
+	}
+	return true
 }
--- day02/puzzle2.go
+++ day02/puzzle2.go
@@ -2,8 +2,57 @@ package day02
 
 import (
 	"fmt"
+
+	aocutil "aoc/util"
 )
 
 func Puzzle2() {
 	fmt.Println("Day 2 / Puzzle 2!")
+
+	l := aocutil.Day02Input("day02/input.txt")
+
+	total := 0
+
+	for _, v := range l {
+		if dampenedSafe(v) {
+			total += 1
+		}
+	}
+
+	fmt.Printf("\tAnswer = %d\n", total)
+}
+
+func dampenedSafe(input []int) bool {
+	stepUp := input[0] < input[1]
+	damp := false
+
+	for i := range input {
+		if i+1 == len(input) {
+			break
+		}
+		if stepUp {
+			if input[i] > input[i+1] {
+				if damp {
+					return false
+				}
+				damp = true
+			}
+			diff := input[i+1] - input[i]
+			if diff < 1 || diff > 3 {
+				return false
+			}
+		} else {
+			if input[i] < input[i+1] {
+				if damp {
+					return false
+				}
+				damp = true
+			}
+			diff := input[i] - input[i+1]
+			if diff < 1 || diff > 3 {
+				return false
+			}
+		}
+	}
+	return true
 }
